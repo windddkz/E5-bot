dbddcca4903f71351b95b1853895c9cd184104de
windddk <windddk@163.com>
2020-05-23 22:04:34 -0400

    add notify emaill

--- api_bot.py
+++ api_bot.py
@@ -11,26 +11,26 @@ def check_mailbox(account):
     print("check mail box done")
 
 
+def send_notify_email(account, content):
+    print("begin send email...")
+    m = account.new_message()
+    m.to.add(os.environ["NOTIFY_EMAIL"])
+    m.subject = 'Testing E5-bot'
+    m.body = "This is a test email from github workflow, check it please" + "\r\n" + content
+    m.send()
+    print("send email done...")
+
+
 def check_onedrive(account):
     print("check onedrive")
-    storage = account.storage()  # here we get the storage instance that handles all the storage options.
-    drives = storage.get_drives()
-    print(drives)
+    storage = account.storage()
     my_drive = storage.get_default_drive()  # or get_drive('drive-id')
     root_folder = my_drive.get_root_folder()
+    onedrive_contents = ""
     for item in root_folder.get_items(limit=25):
-        print(item)
-        if item.is_folder:
-            print(item.get_items(2))  # print the first to element on this folder.
-        elif item.is_file:
-            if item.is_photo:
-                print(item.camera_model)  # print some metadata of this photo
-            elif item.is_image:
-                print(item.dimensions)  # print the image dimensions
-            else:
-                # regular file:
-                print(item.mime_type)
+        onedrive_contents += str(item) + "\r\n"
     print("check onedrive done")
+    return onedrive_contents
 
 
 if __name__ == "__main__":
@@ -44,4 +44,6 @@ if __name__ == "__main__":
         account.authenticate()
 
     check_mailbox(account)
-    check_onedrive(account)
+    content = check_onedrive(account)
+    send_notify_email(account, content)
+
